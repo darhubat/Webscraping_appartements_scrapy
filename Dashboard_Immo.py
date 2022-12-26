cf20273f0de84302c0579de5664e5101aa647c03
darhubat <79753845+darhubat@users.noreply.github.com>
2022-12-26 16:18:10 +0100

    Bei Datengewinnung, Bereinigung und Anreicherung die Performance verbessert und div. Bugs bereinigt.

--- Dashboard_Immo.py
+++ Dashboard_Immo.py
@@ -8,16 +8,18 @@ from dash import dash_table
 import datetime as dt
 
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, meta_tags=[{'name': 'viewport','content': 'width=device-width, initial-scale=1.0'}])
 # für Heroku-Server ist die untenstehende Zeile notwendig
 server = app.server
 
 
-df = pd.read_csv(r'output_clean\\appartements_clean_lat_lon.csv', delimiter=',', parse_dates=True)
+df = pd.read_csv(r'output\\appartements_bereinigt.csv', delimiter=',', parse_dates=True)
 # Datenbereinigung Schritt 1
 df['Datum'] = pd.to_datetime(df['Datum'], errors='coerce').dt.normalize()
-df.rename(columns = {'Latitude/Longitude': 'Koordinaten'}, inplace = True)
-df[['latitude', 'longitude']] = df['Koordinaten'].str.extract(pat = '(-?\d+\.\d+),\s*(-?\d+\.\d+)')
+df['Verkaufspreis'] = pd.to_numeric(df['Verkaufspreis'], errors='coerce').astype('Int64').fillna(0.0)
+del df['Latitude/Longitude']
+# df.rename(columns = {'Latitude/Longitude': 'Koordinaten'}, inplace = True)
+# df[['latitude', 'longitude']] = df['Koordinaten'].str.extract(pat = '(-?\d+\.\d+),\s*(-?\d+\.\d+)')
 # df['Jahr'] = df['Datum'].dt.year
 # df['Monat'] = df['Datum'].dt.month
 
@@ -44,7 +46,7 @@ app.layout = html.Div(children=[
                 dcc.Checklist(id='Kanton',
                     options=[
                     # die Labels könnte man auch über eine Funktion als Liste automatisch generieren
-                    {"label": kant, "value": kant} for kant in df.Kanton.unique()],
+                    {"label": kant, "value": kant} for kant in sorted(df.Kanton.unique())],
                     inline=True,
                     style={"padding": "10px", "max-width": "800px", "margin": "auto"},
                     value=[kant for kant in df.Kanton.unique()], )]),
@@ -53,7 +55,7 @@ app.layout = html.Div(children=[
                 dcc.Dropdown(id='Zimmer_Anzahl',
                  options=[
                      # die Labels könnte man auch über eine Funktion als Liste automatisch generieren
-                     {"label": zimmer, "value": zimmer} for zimmer in df.Zimmeranzahl.unique()
+                     {"label": zimmer, "value": zimmer} for zimmer in sorted(df.Zimmeranzahl.unique())
                  ],
                  multi=True,
                  optionHeight=50,
@@ -122,7 +124,8 @@ def update_graph(option_slctd_wohnungsart, option_slctd_kanton, option_slctd_zim
 
     #
     fig_2 = px.scatter_3d(dff, x='Zimmeranzahl', y='Wohnungsgrösse_m2', z='Verkaufspreis',
-                          color='Wohnungsgrösse_m2', size='Verkaufspreis', opacity=0.4, size_max=8) # symbol='Zimmeranzahl'
+                          color='Verkaufspreis', size='Wohnungsgrösse_m2', opacity=0.4, size_max=12, color_continuous_scale='solar',
+                          hover_data=['Wohnungs_Adresse', 'Wohnungsart', 'Wohnungsgrösse_m2', 'Zimmeranzahl', 'Verkaufspreis'])
 
     fig_2.update_layout(
             title='3D Scatterplot der Wohnungs-Verkaufspreise in der Schweiz',
@@ -131,10 +134,10 @@ def update_graph(option_slctd_wohnungsart, option_slctd_kanton, option_slctd_zim
     )
 
     # Map
-    fig_1 = px.scatter_geo(dff, lat='latitude', lon='longitude',
+    fig_1 = px.scatter_geo(dff, lat='Latitude', lon='Longitude',
                           geojson='geometry',
                           scope='europe',
-                          hover_data=['Wohnungsart', 'Wohnungsgrösse_m2', 'Zimmeranzahl', 'Verkaufspreis'],
+                          hover_data=['Wohnungs_Adresse', 'Wohnungsart', 'Wohnungsgrösse_m2', 'Zimmeranzahl', 'Verkaufspreis'],
                           opacity=0.4, projection='orthographic',
                           color_continuous_scale="solar",
                           # plasma, Bluered_r, aggrnyl, brwnyl, deep, thermal, orrd, redor, gray, temps, reds, ylorrd
--- Daten_Bereinigung.py
+++ Daten_Bereinigung.py
@@ -1,44 +1,32 @@
 import pandas as pd
-import geopandas
 import geopy
 import geopy.geocoders as geo
 from geopy.extra.rate_limiter import RateLimiter
-import time
-import glob
-import os
-import numpy as np
 
-""""
-# merging the files
-files_joined = os.path.join('C:\\Users\\dhuab\\PycharmProjects\\Python_Kaufpreise_Immobilien\\output', "appartements*.csv")
 
-# Return a list of all joined files
-list_files = glob.glob(files_joined)
 
-# Merge files by joining all files
-dataframe = pd.concat(map(pd.read_csv, list_files), ignore_index=True)
-print(dataframe)
-"""
+# Einlesen der Daten
+df = pd.read_csv(r'output\appartements_bereinigt.csv', delimiter=',', parse_dates=True)
+
 
 # Bereinigung/Umwandlung der neu gescrapten und ergänzten Daten
-df = pd.read_csv(r'output\appartements_bereinigt.csv', delimiter=',', parse_dates=True)
-anz = len(df)
-for zeile in df[['clean']].iloc[:anz].iterrows():
-    if str(zeile[1][0]) == 'clean':
-        continue
-    else:
-        df['Zimmeranzahl'] = df['Zimmeranzahl'].str.extract('(\d.?\d?)', expand=True)
-        df['Zimmeranzahl'] = df['Zimmeranzahl'].str.replace(",", ".")
-        df['Wohnungsgrösse_m2'] = df['Wohnungsgrösse_m2'].str.extract('(\d*)', expand=True)
-        df['Verkaufspreis'] = df['Verkaufspreis'].str.extract('(\d+\s\d*\s?\d*)', expand=True)
-        df['Verkaufspreis'] = df['Verkaufspreis'].str.replace(" ", "").fillna(0)
-        df['Wohnungsart'] = df['Wohnungsart'].fillna('unbekannt')
-        df['Kanton'] = df['Wohnungs_Adresse'].str[-2:]
-        df['Datum'] = pd.to_datetime(df['Datum'], errors='coerce').dt.normalize()
-        df['Zimmeranzahl'] = df['Zimmeranzahl'].astype(float).fillna(0)
-        df['Wohnungsgrösse_m2'] = pd.to_numeric(df['Wohnungsgrösse_m2'], errors='coerce').astype('Int64').fillna(0)
-        df['Verkaufspreis'] = pd.to_numeric(df['Verkaufspreis'], errors='coerce').astype('Int64').fillna(0)
-        df.insert(loc=len(df.columns), column='clean', value='clean')
+unbereinigt = df['clean'].isna()
+df.loc[unbereinigt, 'Zimmeranzahl'] = df['Zimmeranzahl'].str.extract('(\d.?\d?)', expand=False)
+df.loc[unbereinigt, 'Zimmeranzahl'] = df['Zimmeranzahl'].str.replace(",", ".")
+df['Zimmeranzahl'] = df['Zimmeranzahl'].astype(float).fillna(0)
+df.loc[unbereinigt, 'Wohnungsgrösse_m2'] = df['Wohnungsgrösse_m2'].str.extract('(\d*)', expand=False)
+df['Wohnungsgrösse_m2'] = pd.to_numeric(df['Wohnungsgrösse_m2'], errors='coerce').astype('Int64').fillna(0)
+df.loc[unbereinigt, 'Verkaufspreis'] = df['Verkaufspreis'].str.extract('(\d+\s\d*\s?\d*)', expand=False)
+df.loc[unbereinigt, 'Verkaufspreis'] = df['Verkaufspreis'].str.replace(" ", "")
+df['Verkaufspreis'] = pd.to_numeric(df['Verkaufspreis'], errors='coerce').astype('Int64').fillna(0)
+df.loc[unbereinigt, 'Wohnungsart'] = df['Wohnungsart'].fillna('unbekannt')
+df.loc[unbereinigt, 'Kanton'] = df['Wohnungs_Adresse'].str[-2:]
+df['Datum'] = pd.to_datetime(df['Datum'], errors='coerce').dt.normalize()
+drop_art_index = df[df['Wohnungsart'] == 'Wohnungsart'].index
+df.drop(drop_art_index, inplace=True)
+drop_index = df[df['Zimmeranzahl'] > 11].index
+df.drop(drop_index, inplace=True)
+df.loc[unbereinigt, 'clean'] = 'clean'
 
 
 # Ergänzen von Location-Daten (Long/Lat) aufgrund der Adresse
@@ -46,6 +34,20 @@ geolocator = geo.Nominatim(user_agent='Daten_Bereinigung')
 geo.options.default_timeout = 10
 geocode = RateLimiter(geolocator.geocode, min_delay_seconds=2)
 
+
+# Schaut, welche Zeilen noch keine Langitude & Longitude haben und dann werden nur für diese die Lat & Lon generiert
+missing = df['Latitude'].isna() & df['Longitude'].isna()
+locations = df.loc[missing, 'Wohnungs_Adresse'].apply(geolocator.geocode, timeout=30)
+df.loc[missing, 'Latitude'] = locations.apply(lambda location: location.latitude if location else 'unbekant')
+df.loc[missing, 'Longitude'] = locations.apply(lambda location: location.longitude if location else 'unbekannt')
+
+
+# Schreibt die bereinigten und neu generierten Daten wieder in die CSV-Datei
+df.to_csv(r'output\appartements_bereinigt.csv', sep=',', index=False)
+# df.to_csv(r'output_clean\appartements_bereinigt_clean_lat_long.csv', sep=',', index=False)
+
+"""
+# Alter Code
 def eval_results(x):
     try:
         return (x.latitude, x.longitude)
@@ -61,20 +63,4 @@ for row in df[['Latitude/Longitude']].iloc[:anzahl_zeilen_df].iterrows():
         df['Latitude/Longitude'] = df['Wohnungs_Adresse'].apply(geolocator.geocode, timeout=30).apply(
         lambda x: eval_results(x))
 df.to_csv(r'output_clean\appartements_clean_lat_lon.csv', sep=',', index=False)
-
-
-"""
-df['Latitude/Longitude'] = df['Wohnungs_Adresse'].apply(geolocator.geocode, timeout=100).apply(
-    lambda x: eval_results(x))
-df.to_csv(r'output\appartements_bereinigt.csv', sep=',', index=False)
-"""
-
-"""
-def divide (a, b):
-    if str(a) != 'nan':
-        return a
-    else:
-        b.apply(geolocator.geocode, timeout=30).apply(lambda x: eval_results(x))
-
-df['Latitude/Longitude'] = np.vectorize(divide(df['Latitude/Longitude'], df['Wohnungs_Adresse']))
 """

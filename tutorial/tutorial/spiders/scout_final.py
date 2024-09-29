72e7236666d13801ca6f72db233d785cea53b141
darhubat <79753845+darhubat@users.noreply.github.com>
2024-09-29 17:24:05 +0200

    css-response auf aktuelle Webseite angepasst und aktualisiert am 29.09.2024. Einige Fehlermeldungen sind noch offen und müssen später analysiert werden.

--- tutorial/tutorial/spiders/scout_final.py
+++ tutorial/tutorial/spiders/scout_final.py
@@ -16,28 +16,34 @@ class ScoutSpider(scrapy.Spider):
     }
 
 
+# css-response angepasst auf aktuelle Webseite von immoscout am 29.09.2024
     def parse(self, response):
 
-        for article in response.css('article'):
+        for article in response.css('.ResultList_listItem_j5Td_'):
             try:
                 try:
-                    Wohnungsart = article.css('span.Box-cYFBPY.Badge__StyledBadge-bJJanS.kQjLxE.fdYwUL::text').getall()[0]
+                    # Wohnungsart = article.css('span.Box-cYFBPY.Badge__StyledBadge-bJJanS.kQjLxE.fdYwUL::text').getall()[0]
+                    Wohnungsart = article.css('div.HgNewConstructionBadge_newConstructionBadgeContainer_Ivd9x > span::text').getall()[0],
                 except:
                     Wohnungsart = 'unbekannt'
                 try:
-                    Zimmeranzahl = article.css('h3.Box-cYFBPY::text').extract()[0].strip()
+                    # Zimmeranzahl = article.css('h3.Box-cYFBPY::text').extract()[0].strip()
+                    Zimmeranzahl = article.css('div.HgListingCard_mainTitle_x0p2D > div > strong:nth-child(1)::text').extract()[0].strip(),
                 except:
                     Zimmeranzahl = 'unbekannt'
                 try:
-                    Wohnungsgroesse_m2 = article.css('h3.Box-cYFBPY::text').extract()[2].strip(),
+                    # Wohnungsgroesse_m2 = article.css('h3.Box-cYFBPY::text').extract()[2].strip(),
+                    Wohnungsgroesse_m2 = article.css('div.HgListingCard_mainTitle_x0p2D > div > strong:nth-child(3)::text').extract()[0].strip(),
                 except:
                     Wohnungsgroesse_m2 = 'unbekannt'
                 try:
-                    Verkaufspreis = article.css('h3.Box-cYFBPY > span::text').extract()[0].strip(),
+                    # Verkaufspreis = article.css('h3.Box-cYFBPY > span::text').extract()[0].strip(),
+                    Verkaufspreis = article.css('span.HgListingRoomsLivingSpacePrice_price_u9Vee::text').extract()[0].strip(),
                 except:
                     Verkaufspreis = 'unbekannt'
                 try:
-                    Wohnungsadresse = article.css('span.AddressLine__TextStyled-eaUAMD::text').extract()[0],
+                    # Wohnungsadresse = article.css('span.AddressLine__TextStyled-eaUAMD::text').extract()[0],
+                    Wohnungsadresse = article.css('div.HgListingCard_secondaryTitle_uVla3 > div > address::text').extract()[0],
                 except:
                     Wohnungsadresse = 'unbekannt'
                     # 'Seitenzahl': response.css('a.Box-cYFBPY PseudoBox-fXdOzA Shell-fTlxHA dxURpc gVespY gfKtRI::attribut(int)').extract(),
@@ -54,10 +60,44 @@ class ScoutSpider(scrapy.Spider):
             except:
                 print(f'Crawling {article} fehlgeschlagen')
 
-        next_page = 'https://www.immoscout24.ch/de/wohnung/kaufen/land-schweiz-fl?pn=' + str(ScoutSpider.page_number)
-        if ScoutSpider.page_number <= 44:
-            ScoutSpider.page_number += 1
-            yield response.follow(next_page, callback = self.parse)
+            ScoutSpider.page_number = max(ScoutSpider.page_number, 2)  # Sicherstellen, dass die Seitenzahl mindestens 2 ist
+            next_page = 'https://www.immoscout24.ch/de/wohnung/kaufen/land-schweiz-fl?pn=' + str(
+                ScoutSpider.page_number)
+            if ScoutSpider.page_number <= 60:
+                ScoutSpider.page_number += 1
+                yield response.follow(next_page, callback=self.parse)
+
+""""
+def parse(self, response):
+    for article in response.css('article'):
+        try:
+            # Wohnungsart
+            Wohnungsart = article.css('span.Badge__StyledBadge-bJJanS.kQjLxE::text').get(default='unbekannt')
+
+            # Zimmeranzahl
+            Zimmeranzahl = article.css('h3.Box-cYFBPY::text').get(default='unbekannt').strip()
+
+            # Wohnungsgröße (m²)
+            Wohnungsgroesse_m2 = article.css('span.Box-cYFBPY::text').get(
+                default='unbekannt').strip()  # Stelle sicher, dass der korrekte Index verwendet wird
+
+            # Verkaufspreis
+            Verkaufspreis = article.css('h3.Box-cYFBPY > span::text').get(default='unbekannt').strip()
+
+            # Wohnungsadresse
+            Wohnungsadresse = article.css('span.AddressLine__TextStyled-eaUAMD::text').get(default='unbekannt')
+
+            yield {
+                'Datum': date.today().strftime('%Y-%m-%d'),
+                'Wohnungsart': Wohnungsart,
+                'Zimmeranzahl': Zimmeranzahl,
+                'Wohnungsgröße_m2': Wohnungsgroesse_m2,
+                'Verkaufspreis': Verkaufspreis,
+                'Wohnungs_Adresse': Wohnungsadresse
+            }
+        except Exception as e:
+            self.logger.error(f'Crawling {article} fehlgeschlagen: {e}')
+"""
 
 
 
